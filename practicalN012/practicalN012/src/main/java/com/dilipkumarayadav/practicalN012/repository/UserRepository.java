ccbe6876d77eb99550e015d9b3475c4e90fcbf46
Dilip Kuamr Yadav <yadavdilipkumar533@gmail.com>
2025-10-23 10:14:21 +0530

    Delete practicalN012/practicalN012 directory

--- practicalN012/practicalN012/src/main/java/com/dilipkumarayadav/practicalN012/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dilipkumarayadav.practicalN012.repository;
-
-import com.dilipkumarayadav.practicalN012.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
