eb64ba971c83a27941f634f9022d5402d42bc7b3
Dilip Kuamr Yadav <yadavdilipkumar533@gmail.com>
2025-10-23 09:34:48 +0530

    Add files via upload

---
+++ practicalN012/practicalN012/src/test/java/com/dilipkumarayadav/practicalN012/PracticalN012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilipkumarayadav.practicalN012;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalN012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
